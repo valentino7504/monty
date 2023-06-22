435edbffcf633a47a7f031d456f9e8c32af560b9
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-22 18:54:57 +0100

    Implemented sub

--- monty.h
+++ monty.h
@@ -47,4 +47,5 @@ void pop_op(stack_t **stack, unsigned int line_number);
 void swap_op(stack_t **stack, unsigned int line_number);
 void nop_op(stack_t **stack, unsigned int line_number);
 void add_op(stack_t **stack, unsigned int line_number);
+void sub_op(stack_t **stack, unsigned int line_number);
 #endif
--- opcode_functions2.c
+++ opcode_functions2.c
@@ -29,3 +29,23 @@ void add_op(stack_t **stack, unsigned int line_number)
 	current->n += (*stack)->n;
 	pop_op(stack, line_number);
 }
+/**
+ * sub_op - prints the value at the top of the stack
+ * @stack: the stack to be changed
+ * @line_number: the line number
+ */
+void sub_op(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		free_stack(*stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
+	current = (*stack)->next;
+	current->n = (-1 * current->n) + (*stack)->n;
+	pop_op(stack, line_number);
+}
