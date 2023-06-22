d0f79043606fff0a7eede0a43128cb536bae83a9
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-22 19:25:04 +0100

    Added pchar

--- opcode_functions3.c
+++ opcode_functions3.c
@@ -26,3 +26,26 @@ void mod_op(stack_t **stack, unsigned int line_number)
 	current->n %= (*stack)->n;
 	pop_op(stack, line_number);
 }
+/**
+ * pchar_op - prints the value at the top of the stack
+ * @stack: the stack to be printed from
+ * @line_number: the line number
+ */
+void pchar_op(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		free_stack(*stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		free_stack(*stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*stack)->n);
+}
